212f127d8bb5e0696a55e557a5eb9c560bd9fdfd
user <user@JBTC10110>
2018-08-30 15:58:15 +0900

    RPG

---
+++ RPG/src/monster/Mon.java
@@ -0,0 +1,13 @@
+package monster;
+
+import java.util.*;
+import setting.*;
+
+public class Mon {
+	Va va=new Va();
+	Random rand;
+	int mon=va.Mon[rand.nextInt(10)];
+	int exp=rand.nextInt(va.MExp[mon]);
+	
+	
+}
---
+++ RPG/src/setting/Va.java
@@ -0,0 +1,10 @@
+package setting;
+
+public class Va {
+	public int LV=1;
+	public int HP;
+	public int MP;
+	public int Mon[]= {0,1,2,3,4,5,6,7,8,9};
+	public int MExp[]= {5,15,30,50,75,105,140,180,225,275};
+	public int Exp=LV*5;
+}
